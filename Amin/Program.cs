cd9938830379f77a802e8c02dd0e1b4333046c6e
Amin Affandi <aminaffandi@github.com>
2024-07-23 14:02:56 +0800

    Amin Week 1 Assignment

--- Amin/Program.cs
+++ Amin/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using helloworld.models;
+using helloworld.controller;
+using System.ComponentModel;
+using System.Diagnostics.Tracing;
+using Newtonsoft.Json;
+
+class Program{
+    static void Main (string [] args){
+        System.Console.WriteLine("Choose assignment:");
+        if (args.Length == 0){
+            System.Console.WriteLine("No assignment was choosed");
+            return;
+        }
+        string assignment = args[0].ToLower();
+        switch (assignment){
+            case "week1":
+                System.Console.WriteLine("Week 1 Assignment");
+                Week1Assignment();
+                break;
+            case "week2":
+                System.Console.WriteLine("Week 2 Assignment");
+                Week2Assignment();
+                break;
+            case "week3":
+                System.Console.WriteLine("Week 3 Assignment");
+                Week3Assignment();
+                break;
+            case "week4":
+                System.Console.WriteLine("Week 4 Assignment");
+                Week4Assignment();
+                break;
+            case "week5":
+                System.Console.WriteLine("Week 5 Assignment");
+                Week5Assignment();
+                break;
+            default:
+                System.Console.WriteLine("Invalid input given");
+                break;
+        }
+
+    }
+
+    static void Week1Assignment() {
+        // Week 1 Assignment
+        System.Console.Write("Enter your name:");
+        string name = Console.ReadLine();
+        System.Console.Write("Enter your age:");
+        string agenumber = Console.ReadLine();
+        int age;
+        if (!int.TryParse(agenumber, out age)) {
+            Console.WriteLine("Invalid age input.");
+            return;
+        }
+        System.Console.Write("Unlease your moto! :");
+        string moto = Console.ReadLine();
+
+        Week1Model user = new Week1Model {
+            Name = name,
+            Age = age,
+            Moto = moto,
+        };
+        Week1Controller userController =  new Week1Controller();
+        userController.UserDetails(user);
+    }
+
+}
