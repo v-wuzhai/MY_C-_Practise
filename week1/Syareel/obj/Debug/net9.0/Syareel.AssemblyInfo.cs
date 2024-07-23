8036616f22241c465f8424b09884f1d6d122de4c
Ramanan Arumugam <v-arumugamra@microsoft.com>
2024-07-23 16:08:26 +0800

    Completed week 1 project Ramanan

--- week1/Syareel/obj/Debug/net9.0/Syareel.AssemblyInfo.cs
+++ week1/Syareel/obj/Debug/net9.0/Syareel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Syareel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c750ac4b61a515371dea8509eb05e1b0b80a18ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d610dfbd1695ef449549544ba0135fdfa184b586")]
 [assembly: System.Reflection.AssemblyProductAttribute("Syareel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Syareel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
