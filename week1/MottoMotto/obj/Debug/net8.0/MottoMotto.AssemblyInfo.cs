1f1311e68ba3a1c8882d4308d61bd6e5e86fdc4a
Syahmie Saifulyusri <v-syahmies@microsoft.com>
2024-07-25 10:32:35 +0800

    Moved places for file

--- week1/MottoMotto/obj/Debug/net8.0/MottoMotto.AssemblyInfo.cs
+++ week1/MottoMotto/obj/Debug/net8.0/MottoMotto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MottoMotto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2891d666fb11ff88f81b861687bf9ed5d1368e49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8774df68f2c1ba064b3fe994581d5f9da742d588")]
 [assembly: System.Reflection.AssemblyProductAttribute("MottoMotto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MottoMotto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
