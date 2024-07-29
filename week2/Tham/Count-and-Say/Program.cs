19ac8417b6e28f70fc3ef9866c9fd2be18cae69d
Tham Ye Szann <thamyeszann@beyondsoft.com>
2024-07-29 09:21:56 +0000

    Completed week2

---
+++ week2/Tham/Count-and-Say/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+ 
+class CountAndSay
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the value of n: ");
+        int n = int.Parse(Console.ReadLine());
+        string result = CountAndSaySequence(n);
+        Console.WriteLine($"The {n}th term of the count-and-say sequence is: {result}");
+    }
+ 
+    public static string CountAndSaySequence(int n)
+    {
+        if (n <= 0)
+            return "";
+ 
+        string result = "1";
+ 
+        for (int i = 1; i < n; i++)
+        {
+            result = GenerateNextTerm(result);
+        }
+ 
+        return result;
+    }
+ 
+    private static string GenerateNextTerm(string term)
+    {
+        StringBuilder nextTerm = new StringBuilder();
+        int count = 1;
+ 
+        for (int i = 1; i < term.Length; i++)
+        {
+            if (term[i] == term[i - 1])
+            {
+                count++;
+            }
+            else
+            {
+                nextTerm.Append(count);
+                nextTerm.Append(term[i - 1]);
+                count = 1;
+            }
+        }
+ 
+        nextTerm.Append(count);
+        nextTerm.Append(term[term.Length - 1]);
+ 
+        return nextTerm.ToString();
+    }
+}
---
+++ week2/Tham/Diamond/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+ 
+class DiamondPattern
+{
+    public static void Main(string[] args)
+    {
+        int n = 4;  // Change this to generate a diamond pattern of a different size
+        PrintDiamond(n);
+    }
+ 
+    public static void PrintDiamond(int n)
+    {
+        // Print the upper part of the diamond
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k < (i * 2); k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+ 
+        // Print the lower part of the diamond
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int j = n; j > i; j--)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k < (i * 2); k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
