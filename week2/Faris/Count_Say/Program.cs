c7f89f338c22bd289e0c2a34136807161b877596
Muhammad Faris Afiq Fauzi <v-mufauzi@microsoft.com>
2024-07-30 15:03:23 +0800

    Done Week 2

---
+++ week2/Faris/Count_Say/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static string CountAndSay(int seq)
+    {
+        if (seq == 1) return "1";
+
+        string result = "1";
+
+        for (int i = 2; i <= seq; i++)
+        {
+            string current = "";
+            int count = 1;
+            char say = result[0];
+
+            for (int j = 1; j < result.Length; j++)
+            {
+                if (result[j] == say)
+                {
+                    count++;
+                }
+                else
+                {
+                    current += count.ToString() + say;
+                    count = 1;
+                    say = result[j];
+                }
+            }
+
+            current += count.ToString() + say;
+            result = current;
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        string retry;
+
+        do
+        {
+            Console.Write("Enter sequence : ");
+            int seq = Convert.ToInt32(Console.ReadLine());    //user enter term
+            Console.WriteLine($"Answer : {CountAndSay(seq)}");
+            Console.Write("Retry ? y-Yes n-No : "); //if user want to retry
+            retry = Console.ReadLine();
+            Console.WriteLine("");
+        }
+        while (retry == "y");
+
+    }
+}
---
+++ week2/Faris/README.md
@@ -0,0 +1,6 @@
+Week 2
+
+- Primarily practiced the usage of loops and functions
+- Consist of two tasks
+- Diamond : a project that need to produce a diamond shape made from asterisks, based on users' preferences
+- CountAndSay : a sequence of number produce based on its digits' quantity, and sequence based on users' preferences
