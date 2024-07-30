b008f0222ffe0e031afa85247d6bdba3837f0af9
Amna Sham Shol Aripen (BEYONDSOFT CONSULTING INC) <v-amnasha@microsoft.com>
2024-07-30 06:09:44 +0000

    Amna's Week 2 Training

---
+++ week2/Amna/AmnaWeek2-Training2/AmnaWeek2-Training2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DiamondPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("Enter an odd number:");
+                if (int.TryParse(Console.ReadLine(), out int n) && n % 2 != 0)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        int stars = i <= n / 2 ? 2 * i + 1 : 2 * (n - i - 1) + 1;
+                        int spaces = (n - stars) / 2;
+                        Console.WriteLine(new string(' ', spaces) + new string('*', stars));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid odd number.");
+                }
+
+                Console.WriteLine("Do you want to enter another number? (1 for yes, 2 for no)");
+                string response = Console.ReadLine();
+                if (response != "1")
+                {
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
