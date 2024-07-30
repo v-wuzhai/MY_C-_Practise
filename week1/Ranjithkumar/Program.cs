5714352b28f6972b332fb75954e2c7c83585cb5a
Ranjithkumar Saravanan (BEYONDSOFT CONSULTING INC) <v-rasaravana@microsoft.com>
2024-07-30 16:06:54 +0800

    Completed week1 homework

---
+++ week1/Ranjithkumar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week1RanjithkumarSaravanan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Strive for continuous improvement, instead of perfection.");
+        }
+    }
+}
