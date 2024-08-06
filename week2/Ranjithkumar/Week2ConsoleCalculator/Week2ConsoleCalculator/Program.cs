9fb65e1e798a3239c33d8f944bdde5448d597621
Ranjithkumar Saravanan (BEYONDSOFT CONSULTING INC) <v-rasaravana@microsoft.com>
2024-08-06 18:23:19 +0800

    Completed week2 and week3 homework

---
+++ week2/Ranjithkumar/Week2ConsoleCalculator/Week2ConsoleCalculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueCalculating = true;
+
+            while (continueCalculating)
+            {
+                Console.Clear();
+                Console.WriteLine("Console Calculator\n");
+
+                Console.Write("Enter first number: ");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter an operator (+, -, *, /): ");
+                char op = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                Console.Write("Enter second number: ");
+                double num2 = double.Parse(Console.ReadLine());
+
+                double result = 0;
+
+                switch (op)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 != 0)
+                            result = num1 / num2;
+                        else
+                            Console.WriteLine("Error! Division by zero.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operator");
+                        break;
+                }
+
+                Console.WriteLine($"Result: {result}");
+                Console.Write("Do you want to perform another calculation? (y/n): ");
+                continueCalculating = Console.ReadKey().KeyChar == 'y';
+            }
+        }
+    }
+}
