c81d8540cb4a54ba2aaebba45bd447cce9c9b552
Atif Bin Azmi (BEYONDSOFT CONSULTING INC) <v-abinazmi@microsoft.com>
2024-08-07 08:49:11 +0800

    Correction

--- week3/Atif/TwoSum/Program.cs
+++ week3/Atif/TwoSum/Program.cs
@@ -3,40 +3,22 @@ using System.Collections.Generic;
 
 class Program
 {
-    public static List<int> TwoSum(List<int> numbers, int target)
+    public static List<Tuple<int, int>> TwoSum(List<int> numbers, int target)
     {
-        int left = 0, right = numbers.Count - 1;
+        var result = new List<Tuple<int, int>>();
 
-        while (left < right)
+        for (int i = 0; i < numbers.Count; i++)
         {
-            int currentSum = numbers[left] + numbers[right];
-            // If current sum == target, return left and right
-            if (currentSum == target)
+            for (int j = i + 1; j < numbers.Count; j++)
             {
-                if (left + 1 < right + 1)
+                if (numbers[i] + numbers[j] == target)
                 {
-                    return new List<int> { left + 1, right + 1 };
+                    result.Add(new Tuple<int, int>(i + 1, j + 1));
                 }
-                else
-                {
-                    return new List<int> { right + 1, left + 1 };
-                }
-            }
-            // If current sum < target, then increase the
-            // current sum by moving the left pointer by 1
-            else if (currentSum < target)
-            {
-                left++;
-            }
-            else
-            {
-                // If current sum > target, then decrease the
-                // current sum by moving the right pointer by 1
-                right--;
             }
         }
 
-        return new List<int>();
+        return result;
     }
 
     static void Main(string[] args)
@@ -54,15 +36,18 @@ class Program
         Console.WriteLine("Enter the target number:");
         int target = int.Parse(Console.ReadLine());
 
-        List<int> result = TwoSum(numbers, target);
+        List<Tuple<int, int>> result = TwoSum(numbers, target);
 
         if (result.Count > 0)
         {
-            Console.WriteLine("Index 1: {0},Index 2:{1}", result[0], result[1]);
+            foreach (var pair in result)
+            {
+                Console.WriteLine("Index 1: {0}, Index 2: {1}", pair.Item1, pair.Item2);
+            }
         }
         else
         {
-            Console.WriteLine("There no solution for the target number.");
+            Console.WriteLine("There is no solution for the target number.");
         }
     }
 }
