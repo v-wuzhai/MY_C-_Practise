e37b4e9f9f8ecbecea525b39d01d5914887276dc
Ramanan Arumugam <v-arumugamra@microsoft.com>
2024-08-14 11:31:00 +0800

    push homework week two

---
+++ week2/Ramanan/Calculator/Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Calculator");
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Select operation:");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+
+            Console.WriteLine("Enter choice(1/2/3/4):");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0;
+
+            switch (choice)
+            {
+                case 1:
+                    result = num1 + num2;
+                    Console.WriteLine("{0} + {1} = {2}", num1, num2, result);
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    Console.WriteLine("{0} - {1} = {2}", num1, num2, result);
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    Console.WriteLine("{0} * {1} = {2}", num1, num2, result);
+                    break;
+                case 4:
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero.");
+                    }
+                    else
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("{0} / {1} = {2}", num1, num2, result);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid input.");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
