f1e19af89ffee52230147c29e4ef155fa3ed1c23
Amirul Aiman <v-amirulaidi@microsoft.com>
2024-08-27 17:31:14 +0800

    Completed week 3 Homework

---
+++ week3/amirul.a/amirul.a_ListTwoSum/ListTwoSum.cs
@@ -0,0 +1,28 @@
+﻿namespace MyApplication
+{
+    public class ListTwoSum
+    {
+        public static int[] twoSum(List<int> num, int target)
+        {
+            Dictionary<int, int> numToIndex = new Dictionary<int, int>();
+
+            for (int i = 0; i < num.Count; i++)
+            {
+
+                int nums = num[i];
+                int complement = target - nums;
+
+                if (numToIndex.ContainsKey(complement))
+                {
+
+                    return new int[] { numToIndex[complement] + 1, i + 1 };
+
+                }
+                    numToIndex[nums] = i;
+    
+            }
+                return null;
+        }
+    }
+
+}
